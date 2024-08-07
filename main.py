3c69fd54b09043a9fdffb4189aca377ae526ecae
Xandy77 <domirops@yahoo.com.br>
2024-08-07 15:18:31 -0300

    Versão 1.0

---
+++ main.py
@@ -0,0 +1,11 @@
+# lista de carros
+carros = ['Mitsubichi', 'Ferrari', 'Lamborguini', 'BMW', 'Subaru']
+
+# define o separador
+separador = '-'
+
+# variável que armazena todos os itens da lista em uma string
+lista_carro = separador.join(carros)
+
+# exibe a nova string
+print(lista_carro)
